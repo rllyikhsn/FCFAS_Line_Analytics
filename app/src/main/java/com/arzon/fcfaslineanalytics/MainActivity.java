6dd5ee274695e6ac3019f74316eab57bf138d281
rllyikhsn <angledark68@gmail.com>
2019-05-27 06:23:06 +0700

    Initial commit

---
+++ app/src/main/java/com/arzon/fcfaslineanalytics/MainActivity.java
@@ -0,0 +1,17 @@
+package com.arzon.fcfaslineanalytics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.ndk.CrashlyticsNdk;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
+        setContentView(R.layout.activity_main);
+    }
+}
